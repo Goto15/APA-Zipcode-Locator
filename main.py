e9fa76e6ad8f6e1483168d46292e79609ac7b5dc
GoTo15 <goto15pod@gmail.com>
2020-07-17 11:32:21 -0500

    Initial pass at address parsing

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# from address import AddressParser, Address
+import csv
+
+with open('addresses.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    for row in reader:
+        if(len(row) > 3):
+            tempAddress = ' '.join(row[0: -3])
+            print(tempAddress, row[-2])

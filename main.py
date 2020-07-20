6944cf940f7a5339c9799569438098f4a5a5c1bd
GoTo15 <goto15pod@gmail.com>
2020-07-20 10:22:15 -0500

    Clean up script and comment wording

--- main.py
+++ main.py
@@ -4,28 +4,25 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 
-# Dumbly finds the first digit to the end of the string.
-# Works on most results. [Jul 2020]
+# Dumbly finds the first digit in the string 'City, State Zip' to the end of the
+# string. Works for most results. [Jul 2020]
 def dumb_zip_find(zip):
     for i, char in enumerate(zip):
         if char.isdigit():
             return zip[i:]
 
 
-# Google Maps base URL
+# Google Maps search base URL
 baseURL = 'https://www.google.com/maps/search/'
-
-# Target elements on Google Maps [Jul 2020]
-address_search_box = 'searchboxinput'
-submit_btn = 'searchbox-searchbutton'
 
 # Selenium options. Included chromedriver only supports Chrome 84.
 # You can have it run headless by including: `opts.headless = True`
-# Not running in headless for testing [Jul 2020]
+# [Jul 2020]
 opts = Options()
+opts.headless = True
 driver = webdriver.Chrome(options=opts, executable_path='./chromedriver')
 
-# Create the output file named zip_codes.csv
+# Create output file
 writer = csv.writer(open('zip_codes.csv', 'w'))
 
 with open('addresses.csv', newline='') as csvfile:
@@ -33,33 +30,43 @@ with open('addresses.csv', newline='') as csvfile:
     for row in reader:
         # Checks that there is enough information to have a useful address
         if len(row) > 4:
-            # Since the last three elements in the row list are 'in' 'CITY'
-            # 'TX', I am just culling the 'in' and 'TX' and either removing
-            # or making them more explicit. row[-2] is the city since it
-            # does change throughout the data.
-            # Since there are cities/ neighborhoods that are multiple words I
-            # should create the city based on the elements between 'in' and 'TX'
+            # Since the last three elements in the row list are 'in' 'CITY' 'TX',
+            # I am just culling the 'in' and 'TX' and either removing or making
+            # them more explicit. row[-2] is the city since it does change
+            # throughout the data.
+            # TODO: Handle multi-word cities / neighborhoods
             input_address = '+'.join(row[0: -3]) + '+' + row[-2] + '+' + 'TX'
 
-            # Open a new page with the base + search URL and clear inputs
+            # Constructs the Google Maps search URL to bypass a few Selenium inputs
             driver.get(baseURL + input_address)
 
-            # Putting the timeout here as Selenium needs to wait between
-            # submission, then server communication, and finally
-            # DOM rendering before checking for the resultant zip code
+            # Putting the timeout here as Selenium needs to wait between submission,
+            # then server communication, and finally DOM rendering before checking
+            # for the resultant zip code. Also don't want to get banned from G Maps
             time.sleep(2)
             try:
+                # Checks for Google guesstimate results.
                 result_container = driver.find_elements_by_class_name('section-result-description')
                 if result_container:
                     zip_code = dumb_zip_find(result_container[0].text)
 
+                # Checks for specific address results.
                 if not result_container:
                     result_container = driver.find_elements_by_class_name('section-hero-header-title-subtitle')
                     zip_code = dumb_zip_find(result_container[0].text)
 
+                # TODO: Check for array of likely results. Currently unimplemented
+                # if not result_container[0].text:
+                #     # Need to find the first result and click on it.
+                #     # After that find the first result named ugiz4pqJLAG__primary-text gm2-body-2 and pull the text.
+                #     closest_result = driver.find_elements_by_class_name('section-result')[0]
+                #     closest_result.click()
+                #     time.sleep(1)
+
                 writer.writerow([' '.join(row), zip_code])
             except:
-                print(f'Address could not be found: {baseURL + input_address}')
+                # If we can't parse a zip save the address and the URL for human
+                # troubleshooting.
+                writer.writerow([input_address, baseURL + input_address])
 
-# Just here to gracefully close the webdriver once done.
 driver.quit()
